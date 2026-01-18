b8fda9c30d94c30ce65853597a1fed82586eb062
tmslvk <timofeyselyavkomain@gmail.com>
2026-01-18 21:49:28 +0300

    feat(dto): Dto objects added

---
+++ Contacts.Server/DTO/ContactQueryDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Contacts.Server.DTO
+{
+    public class ContactQueryDTO
+    {
+        public string? Search { get; set; }
+        public int Page { get; set; } = 1;
+        public int PageSize { get; set; } = 10;
+    }
+}
---
+++ Contacts.Server/DTO/ContactUpdateDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Contacts.Server.DTO
+{
+    public class ContactUpdateDTO
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public List<string> PhoneNumbers { get; set; } = new List<string>();
+        public string JobTitle { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
