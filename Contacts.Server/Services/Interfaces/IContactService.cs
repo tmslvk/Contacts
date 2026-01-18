5a6a60e2e401c224d3fcbe6b81667d9d0f092750
tmslvk <timofeyselyavkomain@gmail.com>
2026-01-18 21:53:42 +0300

    feat(services): Interfaces of services added

---
+++ Contacts.Server/Services/Interfaces/IContactService.cs
@@ -0,0 +1,14 @@
+﻿using Contacts.Server.DTO;
+using Contacts.Server.Model;
+
+namespace Contacts.Server.Services.Interfaces
+{
+    public interface IContactService
+    {
+        Task<List<Contact>> GetAll(ContactQueryDTO query, CancellationToken cancellationToken);
+        Task<Contact> GetById(Guid id, CancellationToken cancellationToken);
+        Task<Contact> Create(ContactDTO dto, CancellationToken cancellationToken);
+        Task Update(Guid id, ContactUpdateDTO dto, CancellationToken cancellationToken);
+        Task Delete(Guid id, CancellationToken cancellationToken);
+    }
+}
---
+++ Contacts.Server/Services/Interfaces/IPhoneNumberService.cs
@@ -0,0 +1,8 @@
+﻿namespace Contacts.Server.Services.Interfaces
+{
+    public interface IPhoneNumberService
+    {
+        Task AddPhoneNumber(Guid contactId, string phoneNumber, CancellationToken cancellationToken);
+        Task DeletePhoneNumber(Guid contactId, Guid phoneId, CancellationToken cancellationToken);
+    }
+}
