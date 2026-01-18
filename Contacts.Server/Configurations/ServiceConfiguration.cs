67e0c1a76f6e2e51bb0ba9db653d343d1b3c6265
tmslvk <timofeyselyavkomain@gmail.com>
2026-01-18 21:55:47 +0300

    feat(configuration): added config for services

---
+++ Contacts.Server/Configurations/ServiceConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Contacts.Server.Repositories;
+using Contacts.Server.Repositories.Interfaces;
+using Contacts.Server.Services;
+using Contacts.Server.Services.Interfaces;
+using Contacts.Server.Validators;
+using Microsoft.AspNetCore.Identity;
+
+namespace Contacts.Server.Configurations
+{
+    public static class ServiceConfiguration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IContactService, ContactService>();
+            services.AddScoped<IPhoneNumberService, PhoneNumberService>();
+            services.AddScoped<ContactValidator>();
+
+            services.AddScoped<IContactRepository, ContactRepository>();
+            services.AddScoped<IPhoneNumberRepository, PhoneNumberRepository>();
+
+            return services;
+        }
+    }
+}
