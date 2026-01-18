563fac9d32bded3394c5b5a4a67d08399056cdce
tmslvk <timofeyselyavkomain@gmail.com>
2026-01-18 21:48:34 +0300

    feat(model): Models Added

---
+++ Contacts.Server/Model/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace Contacts.Server.Model
+{
+    public class Contact
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public List<PhoneNumber> PhoneNumbers { get; set; } = new List<PhoneNumber>();
+        public string JobTitle { get; set; }
+        public DateTime BirthDate { get; set; }
+
+    }
+}
---
+++ Contacts.Server/Model/PhoneNumber.cs
@@ -0,0 +1,10 @@
+﻿namespace Contacts.Server.Model
+{
+    public class PhoneNumber
+    {
+        public Guid Id { get; set; }
+        public string Number { get; set; }
+        public Guid ContactId { get; set; }
+        public Contact Contact { get; set; }
+    }
+}
