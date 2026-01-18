4e36f44256443f6f195ebedc7d06d9001ba26a5b
tmslvk <timofeyselyavkomain@gmail.com>
2026-01-18 21:52:01 +0300

    feat(repositories): Repositories implementing interfaces

---
+++ Contacts.Server/Repositories/ContactRepository.cs
@@ -0,0 +1,74 @@
+﻿using Contacts.Server.DTO;
+using Contacts.Server.Exceptions;
+using Contacts.Server.Model;
+using Contacts.Server.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.Server.Repositories
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly ApplicationContext _db;
+
+        public ContactRepository(ApplicationContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<List<Contact>> GetAllAsync(ContactQueryDTO query, CancellationToken cancellationToken)
+        {
+            IQueryable<Contact> contactsQuery = _db.Contacts
+                .AsNoTracking()
+                .Include(n => n.Name)
+                .Include(n => n.JobTitle)
+                .Include(n => n.PhoneNumbers);
+
+            if (!string.IsNullOrWhiteSpace(query.Search))
+            {
+                contactsQuery = contactsQuery.Where(n =>
+                    n.Name.Contains(query.Search) ||
+                    n.JobTitle.Contains(query.Search));
+            }
+
+            contactsQuery = contactsQuery.OrderByDescending(n => n.BirthDate);
+
+            contactsQuery = contactsQuery
+                .Skip((query.Page - 1) * query.PageSize)
+                .Take(query.PageSize);
+
+            return await contactsQuery.ToListAsync(cancellationToken);
+        }
+
+        public async Task<Contact?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+        {
+            return await _db.Contacts
+                .AsNoTracking()
+                .Include(c => c.PhoneNumbers)
+                .FirstOrDefaultAsync(n => n.Id == id, cancellationToken);
+        }
+
+        public async Task AddAsync(Contact contact, CancellationToken cancellationToken)
+        {
+            if (contact.PhoneNumbers.Count > 3)
+                throw new ValidationException("A contact can have no more than 3 phone numbers.");
+
+            await _db.Contacts.AddAsync(contact, cancellationToken);
+            await _db.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task UpdateAsync(Contact contact, CancellationToken cancellationToken)
+        {
+            if (contact.PhoneNumbers.Count > 3)
+                throw new ValidationException("A contact can have no more than 3 phone numbers.");
+
+            _db.Contacts.Update(contact);
+            await _db.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(Contact contact, CancellationToken cancellationToken)
+        {
+            _db.Contacts.Remove(contact);
+            await _db.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
---
+++ Contacts.Server/Repositories/PhoneNumberRepository.cs
@@ -0,0 +1,33 @@
+﻿using Contacts.Server;
+using Contacts.Server.Model;
+using Contacts.Server.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+public class PhoneNumberRepository : IPhoneNumberRepository
+{
+    private readonly ApplicationContext _db;
+
+    public PhoneNumberRepository(ApplicationContext db)
+    {
+        _db = db;
+    }
+
+    public async Task<PhoneNumber?> GetByIdAsync(Guid id, CancellationToken cancellationToken)
+    {
+        return await _db.PhoneNumbers
+            .AsNoTracking()
+            .FirstOrDefaultAsync(p => p.Id == id, cancellationToken);
+    }
+
+    public async Task AddAsync(PhoneNumber phoneNumber, CancellationToken cancellationToken)
+    {
+        await _db.PhoneNumbers.AddAsync(phoneNumber, cancellationToken);
+        await _db.SaveChangesAsync(cancellationToken);
+    }
+
+    public async Task DeleteAsync(PhoneNumber phoneNumber, CancellationToken cancellationToken)
+    {
+        _db.PhoneNumbers.Remove(phoneNumber);
+        await _db.SaveChangesAsync(cancellationToken);
+    }
+}
