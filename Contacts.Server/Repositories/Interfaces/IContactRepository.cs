3334d9c6cb6bbdc5be1af5c95c977adad30d4c6a
tmslvk <timofeyselyavkomain@gmail.com>
2026-01-18 21:51:25 +0300

    feat(repositories): Interfaces of repositories added

---
+++ Contacts.Server/Repositories/Interfaces/IContactRepository.cs
@@ -0,0 +1,14 @@
+﻿using Contacts.Server.DTO;
+using Contacts.Server.Model;
+
+namespace Contacts.Server.Repositories.Interfaces
+{
+    public interface IContactRepository
+    {
+        Task<List<Contact>> GetAllAsync(ContactQueryDTO query, CancellationToken cancellationToken);
+        Task<Contact?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task AddAsync(Contact contact, CancellationToken cancellationToken);
+        Task UpdateAsync(Contact contact, CancellationToken cancellationToken);
+        Task DeleteAsync(Contact contact, CancellationToken cancellationToken);
+    }
+}
---
+++ Contacts.Server/Repositories/Interfaces/IPhoneNumberRepository.cs
@@ -0,0 +1,11 @@
+﻿using Contacts.Server.Model;
+
+namespace Contacts.Server.Repositories.Interfaces
+{
+    public interface IPhoneNumberRepository
+    {
+        Task<PhoneNumber?> GetByIdAsync(Guid id, CancellationToken cancellationToken);
+        Task AddAsync(PhoneNumber phoneNumber, CancellationToken cancellationToken);
+        Task DeleteAsync(PhoneNumber phoneNumber, CancellationToken cancellationToken);
+    }
+}
