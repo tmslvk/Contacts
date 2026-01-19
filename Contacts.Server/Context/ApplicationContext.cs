36142c34ede4bcf3f4b9c59b3d828037e038ad59
tmslvk <timofeyselyavkomain@gmail.com>
2026-01-19 17:23:34 +0300

    feat(db): added separate folder and db seeder

---
+++ Contacts.Server/Context/ApplicationContext.cs
@@ -0,0 +1,55 @@
+﻿using Contacts.Server.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.Server.Context
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<Contact> Contacts { get; set; } = null!;
+        public DbSet<PhoneNumber> PhoneNumbers { get; set; } = null!;
+
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Contact>(entity =>
+            {
+                entity.HasKey(u => u.Id);
+
+                entity.Property(u => u.LastName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(u => u.FirstName)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(u => u.JobTitle)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(u => u.BirthDate)
+                    .IsRequired();
+            });
+
+            modelBuilder.Entity<PhoneNumber>(entity =>
+            {
+                entity.HasKey(p => p.Id);
+
+                entity.Property(p => p.Number)
+                    .IsRequired()
+                    .HasMaxLength(20);
+
+                entity.HasOne(p => p.Contact)
+                    .WithMany(c => c.PhoneNumbers)
+                    .HasForeignKey(p => p.ContactId)
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+        }
+    }
+}
---
+++ Contacts.Server/Context/DbSeeder.cs
@@ -0,0 +1,52 @@
+﻿using Contacts.Server.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Contacts.Server.Context
+{
+    public class DbSeeder
+    {
+        private readonly ApplicationContext _context;
+
+        public DbSeeder(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+            if (_context.Contacts.Any())
+                return;
+
+            var random = new Random();
+
+            for (int i = 1; i <= 20; i++)
+            {
+                var contact = new Contact
+                {
+                    Id = Guid.NewGuid(),
+                    FirstName = $"FirstName{i}",
+                    LastName = $"LastName{i}",
+                    JobTitle = $"JobTitle{i}",
+                    BirthDate = DateTime.Now.AddYears(-random.Next(20, 60))
+                };
+
+                // Добавляем 1-3 номера телефонов
+                int phoneCount = random.Next(1, 4);
+                for (int j = 1; j <= phoneCount; j++)
+                {
+                    var phone = new PhoneNumber
+                    {
+                        Id = Guid.NewGuid(),
+                        Number = $"+7{random.Next(900000000, 999999999)}",
+                        Contact = contact
+                    };
+                    contact.PhoneNumbers.Add(phone);
+                }
+
+                _context.Contacts.Add(contact);
+            }
+
+            _context.SaveChanges();
+        }
+    }
+}
