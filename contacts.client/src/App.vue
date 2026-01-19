d46a1f36e8e053cc1fc8efe66e362c0689d6bc99
tmslvk <timofeyselyavkomain@gmail.com>
2026-01-19 17:33:28 +0300

    feat(client): remade of App.vue

--- contacts.client/src/App.vue
+++ contacts.client/src/App.vue
@@ -1,47 +1,36 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="app">
+    <Feed />
+  </div>
 </template>
 
+<script>
+import Feed from "./components/Feed.vue";
+export default {
+  components: {
+    Feed,
+  },
+};
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+html,
+body {
+  width: 100%;
+  height: 100%;
 }
+.app {
+  background: linear-gradient(
+    352deg,
+    rgba(6, 92, 117, 1) 7%,
+    rgba(39, 62, 189, 1) 42%,
+    rgba(9, 19, 158, 1) 86%
+  );
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+  min-height: 100vh;
+  display: flex;
+  justify-content: center; /* Центрируем Feed по горизонтали */
+  padding: 20px;
+  box-sizing: border-box;
 }
 </style>
