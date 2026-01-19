6b0c3353de01dca34d1c4d9c324181bfee52f4ea
tmslvk <timofeyselyavkomain@gmail.com>
2026-01-19 17:25:41 +0300

    fix(model): fixed

--- Contacts.Server/Model/Contact.cs
+++ Contacts.Server/Model/Contact.cs
@@ -3,7 +3,8 @@
     public class Contact
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
         public List<PhoneNumber> PhoneNumbers { get; set; } = new List<PhoneNumber>();
         public string JobTitle { get; set; }
         public DateTime BirthDate { get; set; }
